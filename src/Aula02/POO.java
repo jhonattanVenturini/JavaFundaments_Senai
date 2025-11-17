3c1df1c98a755a6b8e6d8dd2562394ab6ba75b25
JhonattanVenturini <jhonattan.venturini@outlook.com>
2025-11-17 17:15:11 -0300

    feat: add constructor to create multiple Movie objects

--- src/Aula02/POO.java
+++ src/Aula02/POO.java
@@ -1,33 +1,24 @@
 package Aula02;
 
-/*
-* Classes Objetos são "modelos" pra criar as intâncias.
-* Estas classes contem as variaveis que caracterizam o objeto.
-* Metodo(s) Construtor(es) -> possuem as intruções para cirar a "instância"
-* Metodos próprios do objeto:
- */
 public class POO {
-    // Exemplo -> Ficaha de cadastro de Filmes
-    // Primeiramente declaramos as variaveis do objeto(atributos)
-
 
    String nomeFilme;
    int anoLancamento;
    String genero;
-   int notaCritica;
+   double notaCritica;
 
-   //Construtor
-   public String getNomeFilme(){
-      return nomeFilme;
+   // --- Construtor ---
+   public POO(String nomeFilme, int anoLancamento, String genero, double notaCritica) {
+      this.nomeFilme = nomeFilme;
+      this.anoLancamento = anoLancamento;
+      this.genero = genero;
+      this.notaCritica = notaCritica;
    }
-   public int getAnoLancamento (){
-      return anoLancamento;
-   }
-   public String getGenero(){
-      return genero;
-   }
-   public int getnotaCritica(){
-      return notaCritica;
-   }
+
+   // Getters
+   public String getNomeFilme() { return nomeFilme; }
+   public int getAnoLancamento() { return anoLancamento; }
+   public String getGenero() { return genero; }
+   public double getnotaCritica() { return notaCritica; }
 
 }
--- src/Aula02/Teste.java
+++ src/Aula02/Teste.java
@@ -3,21 +3,23 @@ package Aula02;
 public class Teste {
 
     public static void main(String[] args) {
-        POO instancia1 = new POO();
+        // Criando vários filmes com o construtor
+        POO filme1 = new POO("Piratas do Caribe", 2007, "Aventura", 8.0);
+        POO filme2 = new POO("Blade Runner 2049", 2017, "Ficção Científica", 9.0);
+        POO filme3 = new POO("Homem-Aranha 2", 2004, "Ação", 8.5);
 
-        instancia1.nomeFilme = "Piratas do Caribe";
-        instancia1.anoLancamento = 2007;
-        instancia1.genero = "Aventura";
-        instancia1.notaCritica = 8;
+        exibirFilme(filme1);
+        exibirFilme(filme2);
+        exibirFilme(filme3);
+    }
 
-        System.out.println();
+    // Criando um método para imprimir (melhor organização)
+    public static void exibirFilme(POO filme) {
         System.out.println("------------------------------");
-        System.out.println( "Nome : " + instancia1.getNomeFilme());
-        System.out.println( "Ano de Lançamento : " + instancia1.getAnoLancamento());
-        System.out.println( "Genero: " + instancia1.getGenero());
-        System.out.println( "Nota Critica : " + instancia1.getnotaCritica());
+        System.out.println("Nome : " + filme.getNomeFilme());
+        System.out.println("Ano de Lançamento : " + filme.getAnoLancamento());
+        System.out.println("Gênero: " + filme.getGenero());
+        System.out.println("Nota Crítica : " + filme.getnotaCritica());
         System.out.println("------------------------------");
-
-
     }
 }
