3e10e5e1fb6215a4723ef6a908098b4b99c1ee1d
jhonattanVenturini <jhonattan.venturini@outlook.com>
2025-11-19 14:42:18 -0300

    feat: add lesson on loops in Java (for and while)

---
+++ src/Aula04/LoopWhile.java
@@ -0,0 +1,13 @@
+package Aula04;
+
+public class LoopWhile {
+
+    public static void main(String[] args) {
+
+        int opcao = 0, saida = 0;
+         while (){
+             System.out.println();
+         }
+
+    }
+}
